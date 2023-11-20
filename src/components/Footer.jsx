bbb5c0d72e7dc2023fdaef970fb8648ad176c6e7
Lestariayu99 <Lestariayu0801@gmail.com>
2023-11-20 14:14:03 +0700

    mengubah design footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,27 +3,56 @@ import React from 'react';
 const Footer = () => {
  
   return (
-    <footer className="bg-dark text-light py-4">
-      <div className="container text-center">
-        <div className="row">
-          <div className="col-md-6">
-            <h4>Info Kontak</h4>
-            <p>Jl. Dr. Cipta Mangunkusumo no. 66</p>
-            <p>Email: maroonfood@gmail.com</p>
-            <p>Telp: (123) 123 123</p>
-          </div>
-          <div className="col-md-6">
-            <h4>Tautan</h4>
-            <ul className="list-unstyled">
-              <li><a href="#">Tentang Kami</a></li>
-              <li><a href="#">Produk</a></li>
-              <li><a href="#">Kontak</a></li>
-            </ul>
+
+<>
+<footer className="bg-dark text-light py-4">
+        <div className=" d-flex py-5">
+          <div className="container">
+            <div className="row row-cols-2  row-cols-md-5">
+               <div className="col ">
+                <div>
+                  <div className="fw-bold fs-4 mb-3 pt-3 pt-md-0">About</div>
+                  <div className="mb-1">About Us</div>
+                  <div className="mb-1">Features</div>
+                  <div className="mb-1">News</div>
+                  <div className="mb-1">Menu</div>
+                </div>
+              </div>
+              <div className=" col  ">
+                <div>
+                  <div className="fw-bold fs-4 mb-3 pt-3 pt-md-0">Company</div>
+                  <div className="mb-1">Why Maroon-Food ?</div>
+                  <div className="mb-1">Parter With Us</div>
+                  <div className="mb-1">FAQ</div>
+                  <div className="mb-1">Blog</div>
+                </div>
+              </div>
+              <div className=" col ">
+                <div>
+                  <div className="fw-bold fs-4 mb-3 pt-3 pt-md-0">Support</div>
+                  <div className="mb-1">Account</div>
+                  <div className="mb-1">Support Center</div>
+                  <div className="mb-1">FeedBack</div>
+                  <div className="mb-1">Contact Us</div>
+                  <div className="mb-1">Accesbility</div>
+                </div>
+              </div>
+              <div className=" col ">
+                <div>
+                  <div className="fw-bold fs-4 mb-3 pt-3 pt-md-0">
+                    Get In Touch
+                  </div>
+                  <div className="mb-1">
+                    Questions or feedback? We'd love to hear from you
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </>
   );
-};
+}
 
 export default Footer;
--- src/components/Tag/index.jsx
+++ src/components/Tag/index.jsx
@@ -34,37 +34,3 @@ export default Tag;
 
 
 
-
-
-
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { Badge } from 'react-bootstrap';
-// import { useSelector } from 'react-redux';
-// import { faTag } from '@fortawesome/free-solid-svg-icons'; // Import yang benar
-
-// function Tag({ items, onClick }) {
-//   const products = useSelector((state) => state.products);
-//   return (
-//     <>
-//       {items.map((item, i) => (
-//         <Badge
-//           pill
-//           bg={products.tags.includes(item.name) ? 'warning' : 'secondary'}
-//           style={{ fontSize: '60%', cursor: 'pointer' }}
-//           key={i}
-//           onClick={() => onClick(item.name)}
-//         >
-//           <FontAwesomeIcon icon={faTag} /> {item.name}
-//         </Badge>
-//       ))}
-//     </>
-//   );
-// }
-
-// Tag.propTypes = {
-//   items: PropTypes.array.isRequired,
-// };
-
-// export default Tag;
